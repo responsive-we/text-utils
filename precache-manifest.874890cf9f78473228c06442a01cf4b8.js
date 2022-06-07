bc0c79b5c74bde4839ea7cd0afffb10ff8e6dbe6
responsive-we <siddalmas21@gmail.com>
2022-06-07 15:20:58 +0530

    Updates

---
+++ precache-manifest.874890cf9f78473228c06442a01cf4b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ceb560c871a5734b124",
+    "url": "/text-utils/static/css/main.ec05b03f.chunk.css"
+  },
+  {
+    "revision": "4ceb560c871a5734b124",
+    "url": "/text-utils/static/js/main.4ceb560c.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/text-utils/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "76107ae63b706ffc23b8",
+    "url": "/text-utils/static/js/2.76107ae6.chunk.js"
+  },
+  {
+    "revision": "7d770d2e8699e050de64",
+    "url": "/text-utils/static/js/runtime~main.7d770d2e.js"
+  },
+  {
+    "revision": "6fe356a235d2b0f19bc2c85c8c96eeea",
+    "url": "/text-utils/index.html"
+  }
+];
